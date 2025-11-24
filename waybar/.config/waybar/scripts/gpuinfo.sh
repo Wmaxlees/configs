29817ad9bfb13604c3638c2549853120628389d4
wmaxlees <max.lees@gmail.com>
2025-11-24 15:13:14 -0700

    Cleaner waybar

--- waybar/.config/waybar/scripts/gpuinfo.sh
+++ waybar/.config/waybar/scripts/gpuinfo.sh
@@ -5,4 +5,4 @@ MEM_USED=$(echo "$stats" | awk -F', ' '{print $2}')
 MEM_TOTAL=$(echo "$stats" | awk -F', ' '{print $3}')
 MEM_PERC=$(awk "BEGIN {printf \"%.0f\", ($MEM_USED / $MEM_TOTAL) * 100}")
 
-echo "{\"text\": \"${UTIL}% | ${MEM_PERC}%\", \"tooltip\": \"GPU Load: ${UTIL}%\nVRAM: ${MEM_USED}MiB / ${MEM_TOTAL}MiB\"}"
+echo "{\"text\": \"${UTIL}%  ${MEM_PERC}% \", \"tooltip\": \"GPU Load: ${UTIL}%\nVRAM: ${MEM_USED}MiB / ${MEM_TOTAL}MiB\"}"
